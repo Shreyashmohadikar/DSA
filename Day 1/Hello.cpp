16333317626c7d05a6f24fb016a9fd2b5b01d6dc
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-02-24 01:08:54 +0530

    Day 1

---
+++ Day 1/Hello.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string var;
+    cin >> var;
+    cout<<"Hello, World."<<endl;
+    cout<<var;
+    return 0;
+}
+
