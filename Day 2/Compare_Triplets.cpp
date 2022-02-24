66b4ad613457bfe7f1990f462283c789bd9e4e98
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-02-24 13:44:38 +0530

    Day 2

---
+++ Day 2/Compare_Triplets.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[3];
+    int brr[3];
+    for(int i=0 ; i<3 ; i++){
+        cin>>arr[i];
+    }
+    for(int i=0 ; i<3 ; i++){
+        cin>>brr[i];
+    }
+    
+    int alice=0;
+    int bob=0;
+    for(int i=0 ; i<3 ; i++){
+        if(arr[i]>brr[i]){
+            alice++;
+        }
+        else if(arr[i]<brr[i]){
+            bob++;
+        }
+        else{
+            alice += 0;
+            bob += 0;
+        }
+    }
+    cout<<alice<<" "<<bob;
+    return 0;
+}
