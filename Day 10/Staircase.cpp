1d13de8ba475436fa450fce3c6e4764a4cfa760a
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-03-05 19:13:49 +0530

    D10

---
+++ Day 10/Staircase.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+
+    for(int i=0; i<num; i++){
+        for(int j=0; j<num-i-1; j++){
+            cout<<" ";
+        }
+        
+        for(int j=num; j>=num-i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
