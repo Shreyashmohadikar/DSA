eebf2c6b3530cf0eac2462c334f7ac637dc48e0f
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-05-19 01:21:10 +0530

    new

--- Day 10/Left_rotation.cpp
+++ Day 10/Left_rotation.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
-    
+    int n,r;
+    cin>>n>>r;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<r;i++){
+        int temp = arr[0];
+        for(int j=0;j<n-1;j++){
+            arr[j]=arr[j+1];
+        }
+        arr[n-1]=temp;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
