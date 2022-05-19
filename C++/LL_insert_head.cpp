3bef9a19de071933fc655ee44c18ff53a47748d7
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-05-20 00:03:15 +0530

    new

---
+++ C++/LL_insert_head.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void InsertAtHead(Node* &head, int val){
+    Node* n = new Node(val); //create a new node
+    n->next=head;
+    head = n;
+}
+
+void display(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* head = NULL;
+    int num;
+    cin>>num;
+    for(int i=0; i<num; i++){
+        int val;
+        cin>>val;
+        InsertAtHead(head,val);
+    }
+    display(head);
+    return 0;
+}
