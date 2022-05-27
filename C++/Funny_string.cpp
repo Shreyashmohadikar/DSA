3a5963c3b319858ba6d1f2b8157d67de3096c428
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-05-27 19:22:23 +0530

    funny

--- C++/Funny_string.cpp
+++ C++/Funny_string.cpp
@@ -8,13 +8,14 @@ int main(){
         cin>>str;
         int num = str.size();
         bool flag = true;
-        string r; 
-        for(int i=num-1; i>=0; i--){
-            r[i]= str[num-i-1];
-        }
+        string r = str; 
+        // for(int i=num-1; i>=0; i--){
+        //     r[i]= str[num-i-1];
+        // }
         // for(int i=0; i<num; i++){
         //     cout<<r[i];
         // }
+        reverse(r.begin(), r.end());
         for(int i=0; i<num-1; i++){
             if (abs(str[i] - str[i+1]) != abs(r[i] - r[i+1])){
                 flag = false;
