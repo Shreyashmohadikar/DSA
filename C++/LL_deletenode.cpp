70d08b24d3fe74c71efbe95b6a97b1ad4ca08ce8
Shreyashmohadikar <shreyashmohadikar@gmail.com>
2022-06-06 01:16:11 +0530

    new

---
+++ C++/LL_deletenode.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void InsertAtTail(Node* &head, int val){
+    Node* n = new Node(val); //create a new node
+
+    if(head == NULL){
+        head = n;
+    }
+    else{
+        Node* temp = head; //take temp as iterator
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = n;
+    }
+}
+
+void display(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+void DeleteAtPos(Node* &head, int pos){
+    Node* temp = head;
+    for(int i=0; i<pos-2; i++){
+        temp = temp->next;
+    }
+    Node* t;
+    t = temp->next->next;
+    temp->next = t;
+}
+// void InsertAtPos(Node* &head, int val, int pos){
+//     Node* n = new Node(val);
+//     Node* temp = head;
+//     for(int i=0; i<pos-1; i++){
+//             temp = temp->next;
+//         }
+//         Node* t;
+//         t = temp->next;
+//         temp->next = n;
+//         n->next = t;
+// }
+
+int main(){
+    Node* head = NULL;
+    int num;
+    cin>>num;
+    for(int i=1; i<=num; i++){
+        int val;
+        cin>>val;
+        InsertAtTail(head,val);
+    }
+    int pos;
+    cin>>pos;
+    // InsertAtPos(head,val,pos);
+    DeleteAtPos(head,pos);
+    display(head);
+    return 0;
+}
---
+++ C++/LL_rever.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void InsertAtTail(Node* &head, int val){
+    Node* n = new Node(val); //create a new node
+
+    if(head == NULL){
+        head = n;
+    }
+    else{
+        Node* temp = head; //take temp as iterator
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = n;
+    }
+}
+
+void display(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+int main(){
+    Node* head = NULL;
+    int num;
+    cin>>num;
+    for(int i=1; i<=num; i++){
+        int val;
+        cin>>val;
+        InsertAtTail(head,val);
+    }
+    return 0;
+}
